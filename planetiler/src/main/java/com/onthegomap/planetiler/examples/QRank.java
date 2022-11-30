87a37b99ae7948e9e5a80a50b636d407aa254173
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-12-01 00:32:29 +0100

    Scale down qrank for setSortKey to stay in bounds

--- planetiler/src/main/java/com/onthegomap/planetiler/examples/QRank.java
+++ planetiler/src/main/java/com/onthegomap/planetiler/examples/QRank.java
@@ -65,7 +65,7 @@ public class QRank implements Profile {
       var feature = features.point("qrank");
       feature
         .setZoomRange(0, 14)
-        .setSortKey(-getQRank(sourceFeature.getTag("wikidata")) / 10)
+        .setSortKey(-getQRank(sourceFeature.getTag("wikidata")) / 100)
         .setPointLabelGridSizeAndLimit(
           12, // only limit at z_ and below
           128, // break the tile up into _x_ px squares
