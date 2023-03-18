cd5b79ace9d786453473b0cfe8b89d76434fc143
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2023-03-18 21:18:31 +0100

    Use pmtiles output flag in planetiler

--- planetiler/regenerate-swissmap.sh
+++ planetiler/regenerate-swissmap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 ./mvnw clean package --file standalone.pom.xml
-java -cp target/*-with-deps.jar com.onthegomap.planetiler.examples.SwissMap --download --area=monaco
+java -cp target/*-with-deps.jar com.onthegomap.planetiler.examples.SwissMap --download --area=monaco --output=data/output.pmtiles
 docker run --rm -it -v "$(pwd)/data":/data -p 8080:8080 maptiler/tileserver-gl -p 8080
--- planetiler/swissmap-runworld.sh
+++ planetiler/swissmap-runworld.sh
@@ -4,6 +4,6 @@ java  -cp target/*-with-deps.jar com.onthegomap.planetiler.examples.SwissMap \
   --download --area=planet --bounds=world \
   --download-threads=10 --download-chunk-size-mb=1000 \
   --fetch-wikidata \
+  --output=data/swissmap.pmtiles
   --nodemap-type=array --storage=mmap -Xmx32g 2>&1 | tee logs.txt
-pmtiles convert data/swissmap.mbtiles data/swissmap.pmtiles 2>&1 | tee logs-pmtiles.txt
 rclone copyto data/swissmap.pmtiles r2:swiss-map/swissmap.pmtiles 2>&1 | tee logs-rclone.txt
